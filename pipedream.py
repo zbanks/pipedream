7c4812ddf08b25f0fcc63cafa9f8939e09e80009
Zach Banks <zjbanks@gmail.com>
2018-09-23 17:08:33 -0400

    Iniital implementation, supports combining coplanar adjacent triangles

--- pipedream.py
+++ pipedream.py
@@ -1,117 +1,129 @@
-import numpy
-import stl
-from mpl_toolkits import mplot3d
-from matplotlib import pyplot
+import trimesh
+import networkx
+import itertools
 from collections import defaultdict
 
-def show_mesh(mesh):
-    figure = pyplot.figure()
-    axes = mplot3d.Axes3D(figure)
-
-    axes.add_collection(mplot3d.art3d.Poly3DCollection(mesh.vectors))
-
-    # Auto scale
-    scale = mesh.points.flatten(-1)
-    axes.auto_scale_xyz(scale, scale, scale)
-
-    pyplot.show()
-
 def load_mesh(filename):
-    mesh = stl.mesh.Mesh.from_file(filename)
-    mesh.update_normals()
+    mesh = trimesh.load(filename)
+    mesh.fix_normals()
     return mesh
 
-def mesh_to_edges(mesh):
-    # Map of (v1, v2) -> {"faces": (face_idx1, face_idx2)}
-    # Edges are normalized s.t. v1 <= v2
-    initial_edges = defaultdict(list)
-    for face_idx, edge_list in enumerate(mesh.data["vectors"]):
-        # 1-index faces, use negative to mean edge is inverted
-        face_idx += 1
-        assert len(edge_list) > 2
+def route_string(mesh):
+    # face id -> primary face id
+    facet_map = {x: x for x in range(len(mesh.faces))}
+    for facet in mesh.facets:
+        for face in facet:
+            facet_map[face] = facet[0]
 
-        for i in range(len(edge_list)):
-            v1, v2 = tuple(edge_list[i]), tuple(edge_list[i-1])
-            if v2 < v1:
-                v1, v2 = v2, v1
-                face_idx = -face_idx
-            initial_edges[(v1, v2)].append(face_idx)
+    # (facet id, edge) -> (facet id, edge)
+    connectivity = {}
+    # (face id, edge) -> (face id, edge)
+    facet_edges = {}
+    for face, vertices in enumerate(mesh.faces):
+        assert len(vertices) == 3
+        facet = facet_map[face]
+        for i in range(3):
+            for d in (-1, 1):
+                j = (i + d) % 3
+                k = (i + d * 2) % 3
+                v1, v2, v3 = vertices[i], vertices[j], vertices[k]
+                connectivity[(facet, (v1, v2))] = (facet, (v2, v3))
+                facet_edges[(face, (v1, v2))] = (face, (v2, v3))
 
-    open_edges = []
-    edges = {}
-    for edge, faces in initial_edges.items():
-        assert len(faces) == 2
-        #assert faces[0] * faces[1] < 0, faces
-        edges[edge] = {"faces": tuple(sorted(faces))}
-
-    return edges
-
-def populate_edge_metadata(edges, scale=1.0):
-    assert scale > 0.
-
-    for edge, props in edges.items():
-        props["length"] = scale * sum([(a - b) ** 2 for a, b in zip(*edge)]) ** 0.5
-        props["steps"] = []
-
-    for index, (edge, props) in enumerate(sorted(edges.items(), key=lambda x: x[1]["length"])):
-        props["index"] = index
-        props["name"] = "#{} ({} cm)".format(index, scale)
-
-def calculate_spanning_tree(edges):
-    covered_faces = set()
-    covered_faces.add(1)
-
-    for props in edge.values():
-        if all([abs(f) in covered_faces for f in props["faces"]]):
-            props["in_spanning_tree"] = False
+    graph = networkx.Graph()
+    for face_pair, raw_edge in zip(mesh.face_adjacency, mesh.face_adjacency_edges):
+        f1, f2 = [facet_map[f] for f in face_pair]
+        edge = tuple(sorted(raw_edge))
+        if f1 == f2:
+            # Same facet, so elide the shared edge from connectivity
+            for shared_edge in (edge, edge[::-1]):
+                _f1, adj_edge1 = facet_edges[(face_pair[0], shared_edge)]
+                _f2, adj_edge2 = facet_edges[(face_pair[1], shared_edge)]
+                connectivity[(f1, adj_edge1[::-1])] = (f2, adj_edge2)
+                connectivity[(f2, adj_edge2[::-1])] = (f1, adj_edge1)
+                facet_edges[(_f1, adj_edge1[::-1])] = (face_pair[1], adj_edge2)
+                facet_edges[(_f2, adj_edge2[::-1])] = (face_pair[0], adj_edge1)
         else:
-            props["in_spanning_tree"] = True
-            for f in props["faces"]:
-                covered_faces.add(abs(f))
+            graph.add_edge(f1, f2, shared_edge=edge)
 
-def next_edge(mesh, edge, face_idx):
-    if face_idx < 0:
-        v2, v1 = edge
-    else:
-        v1, v2 = edge
-    edge_list = mesh.data["vectors"]
-    for idx, vedge in enumerate(edge_list):
-        if tuple(vedge) == v2:
-            result = tuple(edge_list[idx-1])
-            assert result != v1
-            return (v1, result)
-    raise Exception(face_idx)
+    # minimum_spanning_tree is overkill, we just need *any* spanning tree
+    for face_pair in networkx.minimum_spanning_tree(graph).edges:
+        f1, f2 = [facet_map[f] for f in face_pair]
+        edge = graph.edges[face_pair]["shared_edge"]
+        # cross strings at each shared edge in the spanning tree
+        for shared_edge in (edge, edge[::-1]):
+            fe1 = connectivity[(f1, shared_edge)]
+            fe2 = connectivity[(f2, shared_edge)]
+            connectivity[(f1, shared_edge)] = fe2
+            connectivity[(f2, shared_edge)] = fe1
+            first_step = fe1
 
-def generate_build_steps(mesh, edges):
+    # Follow the connectivity to produce a stepwise path
+    # Each step is (face, (from_vertex, to_vertex))
+    # The string will pass through each edge twice
     steps = []
-    # Starting edge
-    edge = edges.keys()[0]
+    step = first_step
     while True:
-        assert len(edges[edge]["steps"]) < 2
-        edges[edge]["steps"].append(len(steps))
-        steps.append(edges[edge])
-        if len(steps) == len(edges) * 2:
+        steps.append(step)
+        step = connectivity.pop(step)
+        if step == first_step:
             break
-        use_face_2 = (len(edges[edge]["steps"]) == 2) ^ (edges[edge]["in_spanning_tree"])
-        next_face_idx = edges[edge]["faces"][2 if use_face_2 else 1]
-        edge = next_edge(mesh, edge, next_face_idx)
+    steps.append(step)
+
     return steps
 
-def print_build_steps(steps):
-    for edge in steps:
-        print edge["name"]
+def index_to_name(index):
+    name = ""
+    while True:
+        name = chr(ord('A') + (index % 26)) + name
+        if index < 26:
+            break
+        index //= 26
+        index -= 1
+    return name
+
+def print_steps(mesh, steps, scale=1.0):
+    pipes = {}
+    for _face, edge in steps:
+        if edge in pipes or edge[::-1] in pipes:
+            continue
+        i = len(pipes)
+        head = mesh.vertices[edge[1]]
+        tail = mesh.vertices[edge[0]]
+        length = sum([(a - b) ** 2. for a, b in zip(head, tail)]) ** 0.5
+        length = round(length, 2)
+        pipes[edge] = {
+            "name": index_to_name(i),
+            "length": length,
+            "sort_order": (length, i),
+        }
+    print "Bill of Materials"
+    print "================="
+    for pipe in sorted(pipes.values(), key=lambda x: x["sort_order"]):
+        print "Pipe {:>3}, length={}".format(pipe["name"], pipe["length"])
+
+    print ""
+    print "Assembly Steps"
+    print "=============="
+    for i, (face, edge) in enumerate(steps):
+        flip = False
+        if edge not in pipes:
+            edge = edge[::-1]
+            flip = True
+        pipe = pipes[edge]
+        print "Step {:>3}: {} pipe {}".format(i + 1, ["front of", "back of"][flip], pipe["name"])
+    print "          ...and tie off"
+
 
 def main():
-    m = load_mesh("test/prism.stl")
-    return m
-    #show_mesh(m)
-    edges = mesh_to_edges(m)
-    return edges
-    populate_edge_metadata(edges)
-    calculate_spanning_tree(edges)
-    steps = generate_build_steps(mesh, edges)
-    print_build_steps(steps)
-    return edges, steps
+    #trimesh.util.attach_to_log()
+    #mesh = load_mesh("test/star.stl")
+    mesh = load_mesh("test/dodecahedron.stl")
+    #mesh = load_mesh("test/prism.stl")
+    #mesh = trimesh.primitives.Box()
+    steps = route_string(mesh)
+    print_steps(mesh, steps, scale=1.0)
+    return mesh, steps
 
 if __name__ == "__main__":
-    m = main()
+    mesh, steps = main()
